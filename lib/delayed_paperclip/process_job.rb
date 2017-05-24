2b81559432bd0733edd2434317ada12001cd4418
Stoyan Stoyanov <stoyan@live.bg>
2017-05-24 12:44:02 +0300

    Modify process_job.rb to work with Sidekiq instead of ActiveJob

--- lib/delayed_paperclip/process_job.rb
+++ lib/delayed_paperclip/process_job.rb
@@ -5,7 +5,7 @@ module DelayedPaperclip
   	include Sidekiq::Worker
 
     def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-      ::ProcessJob.perform_async(instance_klass, instance_id, attachment_name.to_s)
+      DelayedPaperclip::ProcessJob.perform_async(instance_klass, instance_id, attachment_name.to_s)
     end
 
     def perform(instance_klass, instance_id, attachment_name)
